770413a2c179b4b9099bc9e357896ee3e84fd3a6
Elias Ablan <eliasablan93@gmail.com>
2023-10-07 16:07:46 -0400

    add password double type validation

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -5,11 +5,18 @@ const RegisterForm = () => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [repeatPassword, setRepeatPassword] = useState('');
 
   const { register } = useContext(AuthContext);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (password !== repeatPassword) {
+      alert('Passwords do not match');
+      setPassword('');
+      setRepeatPassword('');
+      return;
+    }
     register(username, email, password);
   };
 
@@ -41,6 +48,14 @@ const RegisterForm = () => {
           placeholder="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+        />
+      </p>
+      <p className="py-3">
+        <input
+          type="password"
+          placeholder="validate password"
+          value={repeatPassword}
+          onChange={(e) => setRepeatPassword(e.target.value)}
         />
       </p>
       <p className="py-3">
