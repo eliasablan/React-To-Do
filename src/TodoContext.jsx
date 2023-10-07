d6ed396181140d1e3c8184284db806fbaede6481
Elias Ablan <eliasablan93@gmail.com>
2023-10-07 15:33:13 -0400

    TodoContext adjustments

--- src/TodoContext.jsx
+++ src/TodoContext.jsx
@@ -1,39 +1,43 @@
-// import { useQuery } from '@tanstack/react-query';
+import { useState, useEffect, createContext, useContext } from 'react';
 import { useLocalStorage } from './useLocalStorage';
-import { useState, createContext } from 'react';
 import { PropTypes } from 'prop-types';
 
-const url = new URL(import.meta.env.VITE_LOCAL_DJANGO_URL);
-const accesKey = import.meta.env.VITE_ACCESS_KEY;
+import { AuthContext } from './AuthContext';
+
+const todos_url = import.meta.env.VITE_TODOS_URL;
 
 const TodoContext = createContext();
 
 const TodoProvider = ({ children }) => {
+  const { isAuthenticated, accessToken } = useContext(AuthContext);
   const [todos, setTodos] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [searchValue, setSearchValue] = useLocalStorage('SEARCH-V1', '');
 
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  useEffect(() => {
+    getTodos();
+  }, [isAuthenticated]);
+
   const getTodos = () => {
-    try {
+    if (isAuthenticated) {
+      setIsLoading(true);
       const options = {
         headers: {
-          Authorization: `Bearer ${accesKey}`,
+          Authorization: `Bearer ${accessToken}`,
           Origin: '*',
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
         },
       };
-      fetch(url, options)
+      fetch(todos_url, options)
         .then((response) => response.json())
         .then((json) => {
           setTodos(json);
-        })
-        .then(() => {
           setIsLoading(false);
           console.log('getTodos success', todos);
         });
-    } catch (error) {
-      console.log('getTodos error', error);
     }
   };
 
@@ -48,91 +52,54 @@ const TodoProvider = ({ children }) => {
     (todo) => !!todo.finished,
   ).length;
 
-  const isFinished = async (todoId, value) => {
-    try {
-      const options = {
-        method: 'PUT',
-        headers: {
-          Authorization: `Bearer ${accesKey}`,
-          Origin: '*',
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ finished: value }),
-      };
+  const modifyTodo = (todoId, modification) => {
+    setIsLoading(true);
+    const options = {
+      method: 'PUT',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        Origin: '*',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(modification),
+    };
 
-      setIsLoading(true);
-      fetch(new URL(todoId, url), options)
-        .then((response) => {
-          if (response.status !== 200) {
-            setIsLoading(false);
-            throw {
-              status: response.status,
-              message: response.statusText,
-            };
-          }
-          return response.json();
-        })
-        .then(() => {
-          getTodos();
-        });
-    } catch (error) {
-      console.log('isFinished error', error);
-      return error;
-    }
+    fetch(new URL(todoId, todos_url), options)
+      .then((response) => {
+        if (!response.ok) {
+          setIsLoading(false);
+          console.log({
+            message: 'MODIFY_TODO_ERROR',
+            modification,
+          });
+          throw {
+            message: 'MODIFY_TODO_ERROR',
+            modification,
+          };
+        }
+        return response.json();
+      })
+      .then(() => getTodos());
   };
 
-  const completeTodo = (todoId) => isFinished(todoId, true);
+  const completeTodo = (id) => modifyTodo(id, { finished: true });
+  const uncompleteTodo = (id) => modifyTodo(id, { finished: false });
+  const deleteTodo = (id) => modifyTodo(id, { deleted: true });
 
-  const uncompleteTodo = (todoId) => isFinished(todoId, false);
-
-  const deleteTodo = async (todoId) => {
-    try {
-      setIsLoading(true);
-      const options = {
-        method: 'PUT',
-        headers: {
-          Authorization: `Bearer ${accesKey}`,
-          Origin: '*',
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ deleted: true }),
-      };
-
-      fetch(new URL(todoId, url), options)
-        .then((response) => {
-          if (response.status !== 200) {
-            setIsLoading(false);
-            throw {
-              status: response.status,
-              message: response.statusText,
-            };
-          }
-          return response.json();
-        })
-        .then(() => getTodos());
-    } catch (error) {
-      console.log('deleteTodo error', error);
-      return error;
-    }
-  };
-
-  const createTodo = async (newTodo) => {
+  const createTodo = (newTodo) => {
     try {
       setIsLoading(true);
       const options = {
         method: 'POST',
         headers: {
-          Authorization: `Bearer ${accesKey}`,
+          Authorization: `Bearer ${accessToken}`,
           Origin: '*',
-          Accept: 'application/json',
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({ todo: newTodo }),
       };
 
-      fetch(url, options)
+      fetch(todos_url, options)
         .then((response) => {
           if (response.status !== 201) {
             setIsLoading(false);
@@ -145,7 +112,7 @@ const TodoProvider = ({ children }) => {
         })
         .then(() => getTodos());
     } catch (error) {
-      console.log('deleteTodo error', error);
+      console.log('createTodo error', error);
       return error;
     }
   };
